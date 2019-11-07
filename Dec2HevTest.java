b47762d4b8524123ec6e1a55e0001f2e73332f63
Gary Friel <37291577+gfriel2104@users.noreply.github.com>
2019-11-07 14:27:11 +0000

    Create Dec2HevTest.java

---
+++ Dec2HevTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class Dec2HexTestTrue {
+  @Test
+  public void evaluatesExpression() {
+    Dec2Hex dec2hex = new Dex2Hex();
+    int num = dec2hex.input();
+    assertEquals(num > 0);
+  }
+}
