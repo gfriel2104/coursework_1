1fdcd818b3ca2a93b639d19a8006ac6a1c7c53ec
Gary Friel <37291577+gfriel2104@users.noreply.github.com>
2019-11-07 09:02:04 +0000

    Update Dec2Hex.java

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -24,16 +24,22 @@ class Dec2Hex
          int rem, num;
 	num = Arg1;
 	String hexadecimal=""; 
-        System.out.println("Converting the Decimal Value " + num + " to Hex...");
+       
+    	if (num != null) 
+	{
+		System.out.println("Converting the Decimal Value " + num + " to Hex...");
         
-        while(num != 0)
-        {
-	    rem=num%16;
-	    hexadecimal= ch[rem] + hexadecimal;
-            num= num/16;
-        }
-        
-        System.out.println("Hexadecimal representation is : " + hexadecimal);
+		while(num != 0)
+		{
+		    rem=num%16;
+		    hexadecimal= ch[rem] + hexadecimal;
+		    num= num/16;
+		}
 
-    }
+		System.out.println("Hexadecimal representation is : " + hexadecimal);
+	}
+	else 
+	{
+		System.out.println("Number cannot be empty");
+	}
 }
