54e60dd6d4b74943c35361f3e68d32d67229abc5
Gary Friel <37291577+gfriel2104@users.noreply.github.com>
2019-11-07 09:10:24 +0000

    Update Dec2Hex.java v4
    Changing where the error checking is displayed.

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -19,6 +19,10 @@ class Dec2Hex
                 System.exit(1);
             }
         }
+	    else
+	    {
+	    	System.out.println("Must enter a number");
+	    }
 
     	char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
          int rem, num;
