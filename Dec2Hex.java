d09bc8c2c8778e7371690bc74359d9dc7d9c95ba
Gary Friel <37291577+gfriel2104@users.noreply.github.com>
2019-11-07 09:08:20 +0000

    Update Dec2Hex.java v3
    Needed == symbol

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -25,7 +25,7 @@ class Dec2Hex
 	num = Arg1;
 	String hexadecimal=""; 
        
-    	if (num = 0) 
+    	if (num == 0) 
 	{
 		System.out.println("Converting the Decimal Value " + num + " to Hex...");
         
