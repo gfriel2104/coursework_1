19a40133333a3246f18155f119963ed26dcafceb
Gary Friel <37291577+gfriel2104@users.noreply.github.com>
2019-11-07 15:45:28 +0000

    Create Dec2HexTest.java

---
+++ junit-example/test/Dec2HexTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class Dec2HexTest
+{
+  @Test
+  public void evaluateExpression()
+  {
+      Dec2Hex dec2hex = new Dec2Hex();
+      int num = dec2hex.evaluate();
+      assertEquals(num < 200);
+  }
+}
